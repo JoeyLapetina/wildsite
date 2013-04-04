4288ac5503b99d6eae83fe2b49aa4792a2ff33eb
Joey Lapetina <joeylapetina@gmail.com>
2013-04-03 21:58:06 -0500

    working on stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wildsite::Application.config.secret_token = '214a08b22d0ef7ba1e3c449667f3ea321dbdd4cad18f591bb34fdbad8c9a5a87cbb236d988d67de8820b0bb00e9448eba3db27d6fb43408f1d4973a2bae7783a'
