39da9911ecca733820709e17040496768e5a85c3
Joey Lapetina <joeylapetina@gmail.com>
2013-04-07 18:17:33 -0500

    working on whats going on

--- app/helpers/streams_helper.rb
+++ app/helpers/streams_helper.rb
@@ -5,7 +5,7 @@ module StreamsHelper
 		@stream = stream
 		i = item
 		 
-	  @title = i.at_css(@stream.title).text unless @stream.title.empty? || i.at_css(@stream.title).nil?  
+	  @title = i.at_css(@stream.title).text unless @stream.title.empty? || i.at_css(@stream.title).nil? 
 	  @by_line = i.at_css(@stream.by_line).text unless @stream.by_line.empty? || i.at_css(@stream.by_line).nil?  
 	  @body = i.at_css(@stream.body).text unless @stream.body.empty? || i.at_css(@stream.body).nil?  
 	  @picture = i.css(@stream.picture).map { |link| link['src'] }.first unless @stream.picture.empty? || i.css(@stream.picture).nil? 
