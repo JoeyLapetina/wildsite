7a7f6720801e86e3be49ba475e276f132bdf85e8
Joey Lapetina <joeylapetina@gmail.com>
2013-04-18 14:17:45 -0500

    added in a way to see the rank on the form

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -1,3 +1,3 @@
 class Stream < ActiveRecord::Base
-  attr_accessible :body, :active, :replies, :views, :item, :item_link, :picture, :stream_url, :title, :video, :name, :by_line, :site_root, :span, :link_root, :picture_root, :limit, :stream_type
+  attr_accessible :body, :active, :rank, :replies, :views, :item, :item_link, :picture, :stream_url, :title, :video, :name, :by_line, :site_root, :span, :link_root, :picture_root, :limit, :stream_type
 end
