4f4f580adcb179817e5a05b8bd410342e10fad11
Joey Lapetina <joeylapetina@gmail.com>
2013-07-16 21:31:07 -0500

    took out the relation so i can get rid of the stream_id

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -1,4 +1,4 @@
 class Stream < ActiveRecord::Base
-	has_many :items, :foreign_key => 'stream_id', :class_name => "StreamItem"
+	#has_many :items, :foreign_key => 'stream_id', :class_name => "StreamItem"
   attr_accessible :body, :track, :reverse_stream, :game, :use_more, :category, :stream_home_url, :active, :rank, :replies, :views, :item, :item_link, :picture, :stream_url, :title, :video, :name, :by_line, :site_root, :span, :link_root, :picture_root, :limit, :stream_type
 end
