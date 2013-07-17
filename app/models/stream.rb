423ec3ffc8d24fc382eb0417e4c991a4b0f5e7d4
Joey Lapetina <joeylapetina@gmail.com>
2013-07-16 21:57:51 -0500

    f the migrations ill just do it wrong the stream_id is a string deal with it

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -1,4 +1,9 @@
 class Stream < ActiveRecord::Base
 	
   attr_accessible :body, :track, :reverse_stream, :game, :use_more, :category, :stream_home_url, :active, :rank, :replies, :views, :item, :item_link, :picture, :stream_url, :title, :video, :name, :by_line, :site_root, :span, :link_root, :picture_root, :limit, :stream_type
+
+  def items
+  	items = StreamItem.where(stream_id: self.id.to_s)
+  end
+
 end
--- app/models/stream_item.rb
+++ app/models/stream_item.rb
@@ -1,4 +1,8 @@
 class StreamItem < ActiveRecord::Base
 	
   attr_accessible :body, :by_line, :item_link, :picture, :replies, :stream_id, :title, :views
+
+  def stream
+  	stream = Stream.find(self.stream_id.to_s)
+  end
 end
