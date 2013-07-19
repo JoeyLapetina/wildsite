8bb4598a5f8e27db379c390a35a9632272b692d4
Joey Lapetina <joeylapetina@gmail.com>
2013-07-19 14:56:59 -0500

    changed how fast the streams load

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,5 +18,5 @@
 
 function updateStream(stream_id) {
 	 $.getScript('/streams/' + stream_id +'.js');  
-	 setTimeout(function(){updateStream(stream_id)}, 1000000);
+	 setTimeout(function(){updateStream(stream_id)}, 50000);
 }  
