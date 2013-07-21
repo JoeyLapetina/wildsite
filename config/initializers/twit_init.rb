46353325c8a74c98471083b967a690496151ba7e
Joey Lapetina <joeylapetina@gmail.com>
2013-07-20 22:42:59 -0500

    asdf

--- config/initializers/twit_init.rb
+++ config/initializers/twit_init.rb
@@ -5,13 +5,3 @@ Twitter.configure do |config|
   config.oauth_token = "1602193819-5qnGk7LQghJZQMf3snV5TGTcOo0H33Uq2Ts85KT"
   config.oauth_token_secret = "hh4xEC2IRpdxytab4OycOeWcyylTLBG5i41fvAZnpA"
 end
-
-if false
-#use Rack::Block do
-#  ip_pattern '83.255.120.15' do
-  # expressions like '192.0.0.' also available
-#    halt 404
-#  end
-#end
-#run App.new
-end
