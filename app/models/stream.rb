1e715d7639bc2e0d067d814b23387fac79d48a7a
Joey Lapetina <joeylapetina@gmail.com>
2013-07-23 15:38:05 -0500

    added in tweet_names and a delete function for stream_items

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -5,7 +5,7 @@ class Stream < ActiveRecord::Base
   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 	has_many :favorites
-  attr_accessible :body, :track, :is_ready, :reverse_stream, :game, :use_more, :category, :stream_home_url, :active, :rank, :replies, :views, :item, :item_link, :picture, :stream_url, :title, :video, :name, :by_line, :site_root, :span, :link_root, :picture_root, :limit, :stream_type
+  attr_accessible :body, :tweet_name, :track, :is_ready, :reverse_stream, :game, :use_more, :category, :stream_home_url, :active, :rank, :replies, :views, :item, :item_link, :picture, :stream_url, :title, :video, :name, :by_line, :site_root, :span, :link_root, :picture_root, :limit, :stream_type
 
   def items
   	items = StreamItem.where(stream_id: self.id.to_s)
