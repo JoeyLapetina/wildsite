d17a62db09688911bd5869d276bc42b2b6ee0c3d
Joey Lapetina <joeylapetina@gmail.com>
2013-11-02 14:36:32 -0500

    a few updates to the code. Taking out lots of old un-used code

--- config/environment.rb
+++ config/environment.rb
@@ -3,15 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Wildsite::Application.initialize!
-
-if false
-	ActionMailer::Base.smtp_settings = {
-	  :user_name => ENV["SENDGRID_USERNAME"],
-	  :password => ENV["SENDGRID_PASSWORD"],
-	  :domain => 'wildscrape.com',
-	  :address => 'smtp.sendgrid.net',
-	  :port => 587,
-	  :authentication => :plain,
-	  :enable_starttls_auto => true
-	}
-end
