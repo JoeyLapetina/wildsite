cccb34805170f3e745d8f87f27aa2c6ed45a8085
Joey Lapetina <joeylapetina@gmail.com>
2013-11-12 00:14:32 -0600

    made sure the relationship between streams and stream_items exists

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -5,6 +5,7 @@ class Stream < ActiveRecord::Base
   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 	has_many :favorites
+  has_many :stream_items
   attr_accessible :body, :tweet_name, :track, :is_ready, :reverse_stream, :game, :use_more, :category, :stream_home_url, :active, :rank, :replies, :views, :item, :item_link, :picture, :stream_url, :title, :video, :name, :by_line, :site_root, :span, :link_root, :picture_root, :limit, :stream_type
 
   def items
--- app/models/stream_item.rb
+++ app/models/stream_item.rb
@@ -1,6 +1,7 @@
 class StreamItem < ActiveRecord::Base
 	
   attr_accessible :body, :by_line, :item_link, :picture, :replies, :stream_id, :title, :views
+  belongs_to :stream
 
   def stream
   	stream = Stream.find(self.stream_id.to_s)
